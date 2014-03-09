a8eed94d993e43ef74818449e65fea26d9d07109
Hamid Samani <hamid1129@gmail.com>
2014-03-10 01:24:14 +0330

    Implemented simple Answer entity

--- familiarMe/src/main/java/net/familiar/domain/Answer.java
+++ familiarMe/src/main/java/net/familiar/domain/Answer.java
@@ -1,5 +1,70 @@
 package net.familiar.domain;
 
-public class Answer {
+import org.joda.time.DateTime;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+public class Answer extends AbstractDocument<String> {
+
+	@CreatedDate
+	private DateTime createdDate;
+
+	@CreatedBy
+	private User createdBy;
+
+	private String content;
+
+	private int score;
+
+	private boolean accept;
+
+	public Answer(DateTime createdDate, User createdBy, String content,
+			int score, boolean accept) {
+		this.createdDate = createdDate;
+		this.createdBy = createdBy;
+		this.content = content;
+		this.score = score;
+		this.accept = accept;
+	}
+
+	public DateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(DateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public boolean isAccept() {
+		return accept;
+	}
+
+	public void setAccept(boolean accept) {
+		this.accept = accept;
+	}
 
 }
